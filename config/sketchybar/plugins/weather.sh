b309cc54ce9196e18bf4884f5cac4af200496b9d
Shaloc <me@await.moe>
2023-03-27 01:04:56 +0800

    config: add weather warnings from qweather

--- config/sketchybar/plugins/weather.sh
+++ config/sketchybar/plugins/weather.sh
@@ -7,9 +7,12 @@ source $HOME/secret.sh
 
 QWEATHER_API="https://devapi.qweather.com/v7/weather/now?location=${LOCATION}&key=${QWEATHER_API_KEY}"
 QWEATHER_AIR_API="https://devapi.qweather.com/v7/air/now?location=${LOCATION}&key=${QWEATHER_API_KEY}"
+QWEATHER_AIR_API="https://devapi.qweather.com/v7/air/warning?location=${LOCATION}&key=${QWEATHER_API_KEY}"
 QWEATHER_MINUTELY_API="https://devapi.qweather.com/v7/minutely/5m?location=${GEO_LOC}&key=${QWEATHER_API_KEY}"
 weather=$(curl -L -X GET --compressed "${QWEATHER_API}")
 air_quality=$(curl -L -X GET --compressed "${QWEATHER_AIR_API}")
+weather_warnings=$(curl -L -X GET --compressed "${QWEATHER_WARNING_API}")
+
 minutely=$(curl -L -X GET --compressed "${QWEATHER_MINUTELY_API}")
 temp=$(echo $weather | jq -r '.now.temp')
 icon=$(echo $weather | jq -r '.now.icon')
@@ -20,8 +23,12 @@ wind_speed=$(echo $weather | jq -r '.now.windSpeed')
 wind_dir=$(echo $weather | jq -r '.now.windDir')
 aqi=$(echo $air_quality | jq -r '.now.aqi')
 minutely_summary=$(echo $minutely | jq -r '.summary')
+warnings=$(echo $weather_warnings | jq -r '.warning | .[] | .type')
+echo "warings: $warnings"
 
 WEATHER_LABEL="${text} ${temp}(${feels_like})°C"
+WEATHER_ICON=$($HOME/.config/sketchybar/plugins/weather_icons.sh $icon)
+
 if [[ $wind_scale -gt 3 ]]; then
     WEATHER_LABEL="${WEATHER_LABEL} ${wind_dir}${wind_scale}级 ${wind_speed}m/s"
 fi
@@ -31,8 +38,13 @@ fi
 if [[ $minutely_summary != "未来两小时无降水" ]]; then
     WEATHER_LABEL="${WEATHER_LABEL} ${minutely_summary}"
 fi
+if [[ $warnings ]]; then
+    WEATHER_LABEL="${WEATHER_LABEL} 有预警"
+    for warning in $warnings; do
+        WEATHER_ICON="${WEATHER_ICON} $($HOME/.config/sketchybar/plugins/weather_icons.sh $warning)"
+    done
+fi
 
-WEATHER_ICON=$($HOME/.config/sketchybar/plugins/weather_icons.sh $icon)
 echo $WEATHER_ICON
 echo $WEATHER_LABEL
 
